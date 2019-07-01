7e71a4b4d6fe0f2cb63d53268471a210a8e57c52
Shivam <51889577+Shivam-exe@users.noreply.github.com>
2019-07-02 01:37:00 +0530

    Add files via upload

---
+++ box2.js
@@ -0,0 +1,24 @@
+function alls(){
+    document.getElementById('ups').style.display="block";
+    document.getElementById('ups1').style.display="block";
+    document.getElementById('ups2').style.display="block";
+    document.getElementById('downs').style.display="block";
+    document.getElementById('downs1').style.display="block";
+    document.getElementById('downs2').style.display="block";
+}
+function reds(){
+    document.getElementById('ups').style.display="block";
+    document.getElementById('ups1').style.display="block";
+    document.getElementById('ups2').style.display="block";
+    document.getElementById('downs').style.display="none";
+    document.getElementById('downs1').style.display="none";
+    document.getElementById('downs2').style.display="none";
+}
+function greens(){
+    document.getElementById('ups').style.display="none";
+    document.getElementById('ups1').style.display="none";
+    document.getElementById('ups2').style.display="none";
+    document.getElementById('downs').style.display="block";
+    document.getElementById('downs1').style.display="block";
+    document.getElementById('downs2').style.display="block";
+}
